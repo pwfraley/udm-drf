66770db19378e39be96a6176f76a52a8dd4ba2fd
Patrick Fraley <patrick@fraley.de>
2023-06-05 22:58:14 +0200

    remove exit1

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate --noinput || exit 1
-python manage.py createsuperuser --noinput || exit 1
+python manage.py createsuperuser --noinput
 python manage.py tailwind build
 python manage.py collectstatic --noinput
 exec "$@"
